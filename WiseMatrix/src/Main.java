803ce0e10f2834d693f1f34d3f2f4e66152d680b
nitheism <nitheism@gmail.com>
2014-09-08 01:35:08 +0300

    WiseMatrix algorithm the complexity of the algorithm is in the first comment

---
+++ WiseMatrix/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+public static void main(String[] args) {
+	/*O(n^2) incrasing really fast*/
+
+}
+	public static void looping(int[][]anArray,int Ê){
+		for(int i=0; i<anArray.length; i++){ 
+	        for(int j=0; j<anArray[i].length; i++){
+	            if(anArray[i][j] == Ê){
+	            	
+	            }
+	            else{
+	            	System.out.println("Does not exist!");
+	            }
+	        }
+	    }
+		
+	}
+}
