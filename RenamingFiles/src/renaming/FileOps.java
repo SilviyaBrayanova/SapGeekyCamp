f9dd525fbea203156142b26d234dc115107897f3
nitheism <nitheism@gmail.com>
2014-09-08 19:01:30 +0300

    Final

--- RenamingFiles/src/renaming/FileOps.java
+++ RenamingFiles/src/renaming/FileOps.java
@@ -3,10 +3,10 @@ package renaming;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+
 import org.apache.commons.io.FilenameUtils;
 
 public class FileOps {
@@ -19,18 +19,16 @@ public class FileOps {
 
 	}
 
-	public static void toUpperCase() throws FileNotFoundException {
+	public static void toUpperCase() throws IOException {
 		for (int i = 0; i < listOfFiles.length; i++) {
 			if (listOfFiles[i].isFile()) {
 				String newname = mixCase(listOfFiles[i].getName());
-
-				if (listOfFiles[i].renameTo(new File(folder, newname))) {
+				File renamedFile = new File(folder, newname);
+				if (listOfFiles[i].renameTo(renamedFile)) {
 					String extension = FilenameUtils
 							.getExtension(listOfFiles[i].getName());
-					if (extension.equals("txt") || extension.equals("pdf")
-							|| extension.equals("docx")
-							|| extension.equals("log")) {
-						rewrite(listOfFiles[i]);
+					if (extension.equals("txt") || extension.equals("log")) {
+						rewrite(renamedFile);
 
 						System.out.println("Done");
 					}
@@ -59,22 +57,25 @@ public class FileOps {
 		return sb.toString();
 	}
 
-	public static void rewrite(File file) throws FileNotFoundException {
-		FileReader reader = new FileReader(file.getAbsolutePath());
-		BufferedReader inFile = new BufferedReader(reader);
+	public static void rewrite(File file) throws IOException {
+		FileReader reader = null;
+		BufferedReader inFile = null;
 
 		try {
+			reader = new FileReader(file.getAbsolutePath());
+			inFile = new BufferedReader(reader);
 			FileWriter fwriter = new FileWriter(file.getAbsolutePath());
 			BufferedWriter outw = new BufferedWriter(fwriter);
+
 			while (inFile.readLine() != null) {
 				String line = mixCase(inFile.readLine());
 				outw.write(line);
 			}
-			inFile.close();
 			outw.close();
+			inFile.close();
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-
 	}
 }
