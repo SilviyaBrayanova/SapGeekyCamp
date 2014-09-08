9b83912838eb733aefd5db179b285ce1f179bc5c
nitheism <nitheism@gmail.com>
2014-09-08 18:14:27 +0300

    Almost done

--- RenamingFiles/src/renaming/FileOps.java
+++ RenamingFiles/src/renaming/FileOps.java
@@ -1,7 +1,13 @@
 package renaming;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import org.apache.commons.io.FilenameUtils;
 
 public class FileOps {
 	public static File folder = new File(
@@ -9,33 +15,66 @@ public class FileOps {
 	public static File[] listOfFiles = folder.listFiles();
 
 	public static void main(String[] argv) throws IOException {
+		toUpperCase();
 
 	}
 
-	public static void toUpperCase() {
+	public static void toUpperCase() throws FileNotFoundException {
 		for (int i = 0; i < listOfFiles.length; i++) {
 			if (listOfFiles[i].isFile()) {
-				String name = null;
-				int value = 1;
-				for (int j = 0; j < listOfFiles[i].getName().length(); j++) {
-					if (value == 1) {
-						name += Character.toLowerCase(listOfFiles[i].getName()
-								.charAt(j));
-						value = 2;
+				String newname = mixCase(listOfFiles[i].getName());
+
+				if (listOfFiles[i].renameTo(new File(folder, newname))) {
+					String extension = FilenameUtils
+							.getExtension(listOfFiles[i].getName());
+					if (extension.equals("txt") || extension.equals("pdf")
+							|| extension.equals("docx")
+							|| extension.equals("log")) {
+						rewrite(listOfFiles[i]);
+
+						System.out.println("Done");
 					}
-					if (value == 2) {
-						name += Character.toUpperCase(listOfFiles[i].getName()
-								.charAt(j));
-						value = 1;
-					}
+
 				}
-				if (listOfFiles[i].renameTo(new File(folder,name))) {
-					System.out.println("Done");
-					
-				} else {
-					System.out.println("Nope");
+			} else {
+				System.out.println("Nope");
+			}
+		}
+	}
+
+	public static String mixCase(String in) {
+		StringBuilder sb = new StringBuilder();
+		if (in != null) {
+			char[] arr = in.toCharArray();
+			if (arr.length > 0) {
+				char f = arr[0];
+				boolean first = Character.isUpperCase(f);
+				for (int i = 0; i < arr.length; i++) {
+					sb.append((first) ? Character.toLowerCase(arr[i])
+							: Character.toUpperCase(arr[i]));
+					first = !first;
 				}
 			}
 		}
+		return sb.toString();
+	}
+
+	public static void rewrite(File file) throws FileNotFoundException {
+		FileReader reader = new FileReader(file.getAbsolutePath());
+		BufferedReader inFile = new BufferedReader(reader);
+
+		try {
+			FileWriter fwriter = new FileWriter(file.getAbsolutePath());
+			BufferedWriter outw = new BufferedWriter(fwriter);
+			while (inFile.readLine() != null) {
+				String line = mixCase(inFile.readLine());
+				outw.write(line);
+			}
+			inFile.close();
+			outw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 	}
 }
