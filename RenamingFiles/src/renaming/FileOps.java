2e3930d100ee88dd5b40e4f13050f6bfbbbb88f6
nitheism <nitheism@gmail.com>
2014-09-08 20:21:55 +0300

    Final version

--- RenamingFiles/src/renaming/FileOps.java
+++ RenamingFiles/src/renaming/FileOps.java
@@ -1,12 +1,13 @@
 package renaming;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 import org.apache.commons.io.FilenameUtils;
 
 public class FileOps {
@@ -19,7 +20,7 @@ public class FileOps {
 
 	}
 
-	public static void toUpperCase() throws IOException {
+	public static void toUpperCase() throws FileNotFoundException {
 		for (int i = 0; i < listOfFiles.length; i++) {
 			if (listOfFiles[i].isFile()) {
 				String newname = mixCase(listOfFiles[i].getName());
@@ -31,7 +32,7 @@ public class FileOps {
 					if (extension.equals("txt") || extension.equals("pdf")
 							|| extension.equals("tXt")
 							|| extension.equals("TxT")) {
-						rewrite(renamedFile.getAbsoluteFile());
+						rewrite(renamedFile);
 
 						System.out.println("Done");
 					}
@@ -60,30 +61,21 @@ public class FileOps {
 		return sb.toString();
 	}
 
-	public static void rewrite(File file) throws IOException {
-		FileReader reader = null;
-		BufferedReader inFile = null;
+	public static void rewrite(File file) throws FileNotFoundException {
+		Scanner inFile = new Scanner(file);
+		ArrayList<String> nfr = new ArrayList<String>();
 
 		try {
-			reader = new FileReader(file.getAbsolutePath());
-			inFile = new BufferedReader(reader);
-			FileWriter fwriter = new FileWriter(file.getAbsolutePath());
-			BufferedWriter outw = new BufferedWriter(fwriter);
+			PrintWriter pw = new PrintWriter(new FileOutputStream(file));
+			while (inFile.hasNext()) {
+				pw.println(mixCase(inFile.next()));
 
-			while (inFile.readLine() != null) {
-				String line = mixCase(inFile.readLine());
-				outw.write(line);
 			}
+			pw.close();
 
 		} catch (IOException e) {
 			e.printStackTrace();
-		} finally {
-			if (inFile != null)
-				inFile.close();
 
-			if (reader != null)
-				reader.close();
 		}
-
 	}
 }
